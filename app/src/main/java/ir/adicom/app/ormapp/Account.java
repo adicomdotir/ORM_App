e5fb2f933effa6efe033ccf4e6f5c643031dd4a2
Adicom <adicom.ir@gmail.com>
2017-05-26 18:54:00 +0430

    Add requirement

---
+++ app/src/main/java/ir/adicom/app/ormapp/Account.java
@@ -0,0 +1,38 @@
+package ir.adicom.app.ormapp;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * Created by adicom on 5/26/17.
+ */
+
+
+@DatabaseTable(tableName = "accounts")
+public class Account {
+
+    @DatabaseField(id = true)
+    private String name;
+    @DatabaseField
+    private String password;
+
+    public Account() {
+        // ORMLite needs a no-arg constructor
+    }
+    public Account(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- app/src/main/java/ir/adicom/app/ormapp/MainActivity.java
+++ app/src/main/java/ir/adicom/app/ormapp/MainActivity.java
@@ -2,12 +2,74 @@ package ir.adicom.app.ormapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
+import com.j256.ormlite.support.ConnectionSource;
+
+import java.util.List;
+import java.util.Random;
+
+import static android.R.attr.action;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "Adicom";
+    private static final int MAX_NUM_TO_CREATE = 8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseHelper db = new DatabaseHelper(this);
+        RuntimeExceptionDao<Account, Integer> simpleDao = db.getSimpleDataDao();
+        // query for all of the data objects in the database
+        List<Account> list = simpleDao.queryForAll();
+        // our string builder for building the content-view
+        StringBuilder sb = new StringBuilder();
+        sb.append("Found ").append(list.size()).append(" entries in DB in ").append(action).append("()\n");
+
+        // if we already have items in the database
+        int simpleC = 1;
+        for (Account simple : list) {
+            sb.append('#').append(simpleC).append(": ").append(simple).append('\n');
+            simpleC++;
+        }
+        sb.append("------------------------------------------\n");
+        sb.append("Deleted ids:");
+        for (Account simple : list) {
+            //simpleDao.delete(simple);
+            sb.append(' ').append(simple.getName());
+            Log.i(LOG_TAG, "deleting simple(" + simple.getName() + ")");
+            simpleC++;
+        }
+        sb.append('\n');
+        sb.append("------------------------------------------\n");
+
+        int createNum;
+        do {
+            createNum = new Random().nextInt(MAX_NUM_TO_CREATE) + 1;
+        } while (createNum == list.size());
+        sb.append("Creating ").append(createNum).append(" new entries:\n");
+        for (int i = 0; i < createNum; i++) {
+            // create a new simple object
+            long millis = System.currentTimeMillis();
+            Account simple = new Account("acc" + i, "123");
+            // store it in the database
+            simpleDao.create(simple);
+            Log.i(LOG_TAG, "created simple(" + millis + ")");
+            // output it
+            sb.append('#').append(i + 1).append(": ");
+            sb.append(simple).append('\n');
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                // ignore
+            }
+            Log.e(LOG_TAG, sb.toString());
+        }
     }
 }
